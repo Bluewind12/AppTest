73cd4fe119eb3c91a2d0c34129606905cdc6c9a8
Bluewind12 <zero18030@gmail.com>
2017-12-19 13:30:48 +0900

    計算

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
-//Test　Create by MOMOSE
+//Test Create by MOMOSE
+//同一名称テスト
 /**
  * HelloWorld
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        int i = 10, j = 20;
+
         System.out.println("Hello_World");
+        System.out.println(i + "+" + j + "=" + (i + j));
     }
 }
